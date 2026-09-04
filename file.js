de88539e547f2d30fcf3d92563a947d0db113bf4
Muhammad Essa <yootop383@gmail.com>
2026-09-04 16:08:39 +0100

    Update file.js

--- file.js
+++ file.js
@@ -3,6 +3,6 @@ let p = document.createElement('p');
 let body = document.querySelector('body');
 for(let i = str.length - 1 ;  i >= 0 ; i--)
 {
-  p.textcontent = str[i];
+  p.textcontent = `Array Reverse ${str[i]}`;
 };
 body.append(p);
